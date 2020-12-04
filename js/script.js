f676a24306983afa948d4a179b18b2adbbed2a7d
nicgiordani <nikgiordani@gmail.com>
2020-12-04 14:58:15 +0100

    parte h1 ok

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus: Aggiungere un’immagine presa anch’essa da un data
+
+
+var root = new Vue(
+  { el: '#root',
+    data: {
+    message: 'Hello World',
+  },
+});
